630c2e8ee986cb77ab3e1cc4007488a0a1a52124
Toni Le <toni.le@outlook.de>
2024-02-26 21:51:00 +0100

    Reset + Data Preparation

--- src/SkyFlixWeb/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    moviebaseurl: 'https://api.themoviedb.org/3',
-    movieapikey: 'ee08e6a5e28784f2e1d6fbe94c6b3330'
-};
--- src/SkyFlixWeb/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    moviebaseurl: 'https://api.themoviedb.org/3',
-    movieapikey: ''
-};
